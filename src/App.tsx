1ee8d7b4620f52b3aa124f800643d9b09ac5ea98
Majd Shaheen <majd4.sh@gmail.com>
2025-02-23 14:06:33 +0400

    init routes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
